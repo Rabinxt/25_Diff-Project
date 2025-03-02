908980da05d5b4d57658d719bdb252067530f2b4
Rabinxt <brodebid@gmail.com>
2025-03-02 23:50:26 +0545

    FFirst commit

---
+++ more_details/src/Components/accordian/data.jsx
@@ -0,0 +1,18 @@
+const data = [
+    {
+      id: '1',
+      question: "What is the user's educational background?",
+      answer: "The user is a +2 graduate in Nepal from the science faculty and has a strong interest in the IT field.",
+    },
+    {
+      id: '2',
+      question: "What programming skills does the user have?",
+      answer: "The user knows Python very well, is learning Django, and is making web projects using React and Tailwind CSS.",
+    },
+    {
+      id: '3',
+      question: "What is the user's goal related to business?",
+      answer: "The user wants to start a small clothing brand startup in Kathmandu without government procedures.",
+    },
+  ];
+  
--- more_details/src/Components/index.jsx
+++ more_details/src/Components/index.jsx
@@ -1,3 +1,23 @@
+import { useState } from "react"
+import data from "./data.jsx";
+
+
 export default function Accordian(){
-  return <div>Accordian</div>
+
+    const [selected , setSelected] = useState(null);
+
+     return <div className="wrapper">
+        <div className="accordian">
+            {
+                data && data.length > 0 ? 
+                data.map(dataItem => <div className="item">
+                    <div className="title">
+                        <h3>{dataItem.question}</h3>
+                        <span>+</span>
+                    </div>
+                </div>)
+                : <div>No Data Present</div>
+            }
+        </div>
+            </div>
 }
