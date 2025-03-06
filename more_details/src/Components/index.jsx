d140f13c53558beef05fb188ca1642455104234e
Rabinxt <brodebid@gmail.com>
2025-03-06 23:55:12 +0545

    Updated

--- more_details/src/Components/index.jsx
+++ more_details/src/Components/index.jsx
@@ -8,12 +8,14 @@ export default function Accordian() {
 
     const [selected, setSelected] = useState(null);
 
+    
+
     return <div className="wrapper">
         <div className="accordian">
             {
                 data && data.length > 0 ?
                     data.map(dataItem => <div className="item">
-                        <div className="title">
+                        <div className="title" onClick={() => setSelected(dataItem.id === selected ? null : dataItem.id)}>
                             <h3>{dataItem.question}</h3>
                             <span>+</span>
                         </div>
