03f1110e71eb93a1d74815a96b7f627cdcc5be80
Rabinxt <brodebid@gmail.com>
2025-03-12 10:37:22 +0545

    Completed Star_review

--- star-rating/src/App.jsx
+++ star-rating/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Star_rating from './components/star_rating'
 
 const App = () => {
   return (
     <div>
-      
+      <Star_rating />
     </div>
   )
 }
--- star-rating/src/components/star_rating.jsx
+++ star-rating/src/components/star_rating.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
-import {Fastar} from 'react-icons/fa';
-const star_rating = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+import React, { useState } from 'react'
+import { FaStar } from 'react-icons/fa';
+import './star_rating.css';
+const Star_rating = ({
+    noOfStars = 10
+}) => {
+    const [rating, setRating] = useState(0);
+    const [hover , SetHover] = useState(0);
+
+    const handelClick =(i)=>{
+        setRating(i);
+    }
+    const handelMouseMove =(i)=>{
+        SetHover(i)
+    }
+    const handelMouseLeave = (i)=>{
+        SetHover(rating)
+    }
+    return (
+        <div className="StarRating">
+            {
+                [...Array(noOfStars)].map((_, index) => {
+                    index+=1;
+                    return (
+                        <FaStar
+                            key={index}
+                            className={index <= (hover || rating)? 'active' : 'inactive'}
+                            onClick={()=> handelClick(index)}
+                            onMouseMove={ ()=>handelMouseMove(index)}
+                            onMouseLeave={ ()=> handelMouseLeave(index)}
+                        />)
+                })
+            }
+        </div>
+    )
 }
 
-export default star_rating
+export default Star_rating
